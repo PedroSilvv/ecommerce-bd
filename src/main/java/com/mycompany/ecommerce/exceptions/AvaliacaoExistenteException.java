6b56bdd1ec3faf43c6f4351b1822da98da393958
PedroSilvv <pedrosilvestre800@gmail.com>
2024-09-17 16:31:03 -0300

    creating avaliacao exception

---
+++ src/main/java/com/mycompany/ecommerce/exceptions/AvaliacaoExistenteException.java
@@ -0,0 +1,7 @@
+package com.mycompany.ecommerce.exceptions;
+
+public class AvaliacaoExistenteException extends RuntimeException {
+    public AvaliacaoExistenteException(String message){
+        super(message);
+    }
+}
